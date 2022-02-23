4948c47e403f68a8098488d02daaafcde8942032
Zahra <bghzahra@outlook.com>
2022-02-23 10:35:50 +0000

    added formatting

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -7,13 +7,15 @@
  * @param {string[]} stringArr - array of strings
  * @returns {string} - the shortest string
  */
-function findShortestString(
-stringArr
-)
-{ const shortestString = stringArr[0]
-    for (let str of stringArr) {
-if (str.length < shortestString.length) { shortestString = str
-    }}  return shortestString.length }
+function findShortestString(stringArr) {
+  const shortestString = stringArr[0];
+  for (let str of stringArr) {
+    if (str.length < shortestString.length) {
+      shortestString = str;
+    }
+  }
+  return shortestString.length;
+}
 
 console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
 console.log(findShortestString(["why", "hello", "to", "you"]), "to");
