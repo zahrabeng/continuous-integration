d8e922e67b2e8e72bb4c42a215983212af624861
Zahra <bghzahra@outlook.com>
2022-02-23 10:38:25 +0000

    added linting

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -8,7 +8,7 @@
  * @returns {string} - the shortest string
  */
 function findShortestString(stringArr) {
-  const shortestString = stringArr[0];
+  let shortestString = stringArr[0];
   for (let str of stringArr) {
     if (str.length < shortestString.length) {
       shortestString = str;
